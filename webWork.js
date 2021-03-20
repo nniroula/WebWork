d9b8b6a931f6838d96870aa4070498cdfb174227
Nabin <nkbhaie2017@gmail.com>
2021-03-19 18:58:34 -0600

    add webwork

---
+++ webWork.js
@@ -0,0 +1,27 @@
+/*
+What is HTTP?
+HTTP is a Hypertext Transfer Protocol. It is how browsers and servers communicate.
+What is a URL?
+URL is Universal Resource Locator, which is an address for certain items in the Internet.
+What is DNS?
+DNS is Domain Name Service and it turns hostname into an IP address.
+What is a query string?
+Query string is something that we are searching for in the Internet and goes at the end of the URL.
+What are two HTTP verbs and how are they different?
+The two HTTP verbs are GET and POSt. When a request is GET, there is no side effect, but POST has side effect.
+What is an HTTP request?
+HTTP request that client machine or browser sends to the server machine.
+What is an HTTP response?
+HTTP response is a response that server generates based on the request and includes HTTP protocol version, Reesponse status,
+Code, and a header.
+
+What is an HTTP header? Give a couple examples of request and response headers you have seen.
+HTTP header is a piecce of file that contains more information about request and response. Some examples include
+Content Type(eg html/text), Date/time, cookies, cache etc for Response header and Hostname, Date, Language, cookies etc
+for request header.
+
+What are the processes that happen when you type “http://somesite.com/some/page.html” into a browser?
+It turns site.com into an ip address,
+Conncects this ip address to a local port, i.e on port 80 using http protocol, and
+Ask for /some/page.html.
+*/
